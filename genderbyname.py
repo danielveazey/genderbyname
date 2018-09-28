bc994c47544fbb35ad90c9e73bd7f952b56b51d1
Daniel Veazey <danielveazey@gmail.com>
2018-09-27 22:47:11 -0500

    Extract features from names

--- genderbyname.py
+++ genderbyname.py
@@ -7,8 +7,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 # get dataset; names are strings and genders are listed as 0 for female, 1 for male
 df = pd.read_csv('names_genders.csv')
 
+# break the names up into parts
+def features(name):
+    return f"{name[-1]} {name[-2:]} {name[-3:]}"
+
 # extract features from names
 Xfeatures = np.array(df['Name'])
+for i in range(len(Xfeatures)):
+    Xfeatures[i] = features(Xfeatures[i])
 cv = CountVectorizer()
 X = cv.fit_transform(Xfeatures)
 
@@ -20,7 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # use the Naive Bayes classifier for multinomial models
 clf = MultinomialNB()
-clf.fit(X_train,y_train)
-accuracy = clf.score(X_test,y_test)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
 
 print('Accuracy:', accuracy)
